1626b8a328793a19e7e76b30619199e1a8d2bb3d
yusufseptian <yusufseptian39@gmail.com>
2023-10-06 20:06:46 +0700

    first commit

---
+++ app/Controller/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Controller;
+
+class ProductController
+{
+
+    function categories(string $productId, string $categoryId): void
+    {
+        echo "PRODUCT $productId, CATEGORY $categoryId";
+    }
+}
